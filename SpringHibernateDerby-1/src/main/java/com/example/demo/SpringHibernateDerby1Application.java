fdb920d8bf80e14d0530f7cdc32a784a5674ca3c
deshmupr <deshmupr@192.168.56.1>
2018-06-03 16:54:17 +0530

    initial commit spring

---
+++ SpringHibernateDerby-1/src/main/java/com/example/demo/SpringHibernateDerby1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateDerby1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernateDerby1Application.class, args);
+	}
+	
+}
