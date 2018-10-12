4175fc258bbc2a223312ca2aa73101f455f5e689
Pratap <deshmukhpratap1@gmail.com>
2018-10-12 22:51:08 +0100

    user1: hello

--- SpringHibernateDerby-1/src/main/java/com/example/demo/SpringHibernateDerby1Application.java
+++ SpringHibernateDerby-1/src/main/java/com/example/demo/SpringHibernateDerby1Application.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class SpringHibernateDerby1Application {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(SpringHibernateDerby1Application.class, args);
 	}
 }
